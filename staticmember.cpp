22f956a9ff60a6c895ed5469b63949c24309fc1a
khansa-qonita <khansaqonita420@gmail.com>
2026-05-05 18:24:40 +0700

    menambahkan member initialilze list

--- staticmember.cpp
+++ staticmember.cpp
@@ -12,7 +12,10 @@ public:
     //deklarasi prosedur setID() dan printAll()
     void setID();
     void printAll();
-    
 
-
-}
+    //pembuatan constructor mahasiswa dengan parameter pnama
+    //member initialize list
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
