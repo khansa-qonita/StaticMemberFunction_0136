fea8bcc3c8519b2784ffce3381ff03c09a462f91
khansa-qonita <khansaqonita420@gmail.com>
2026-05-05 19:28:30 +0700

    menambahkan constructor dengan parameter lengkap

--- tugas.cpp
+++ tugas.cpp
@@ -27,42 +27,10 @@ public:
         nama = pNama;
     }
 
-    // 4. Constructor dengan parameter lengkap (ID dan Nama)
+    // Constructor dengan parameter lengkap (ID dan Nama)
     Kendaraan(int pId, string pNama) {
         id = pId;
         nama = pNama;
     }
 
-    // Metode untuk menampilkan informasi kendaraan
-    void tampilkanInfo() {
-        cout << "ID Kendaraan   : " << id << endl;
-        cout << "Nama Kendaraan : " << nama << endl;
-        cout << "---------------------------" << endl;
-    }
-};
-
-int main() {
-    cout << "=== Data Pengelolaan Kendaraan ===" << endl << endl;
-
-    // Inisialisasi menggunakan Constructor 1
-    Kendaraan k1;
-    cout << "Objek 1 (Default):" << endl;
-    k1.tampilkanInfo();
-
-    // Inisialisasi menggunakan Constructor 2
-    Kendaraan k2(101);
-    cout << "Objek 2 (Hanya ID):" << endl;
-    k2.tampilkanInfo();
-
-    // Inisialisasi menggunakan Constructor 3
-    Kendaraan k3("Motor Matic");
-    cout << "Objek 3 (Hanya Nama):" << endl;
-    k3.tampilkanInfo();
-
-    // Inisialisasi menggunakan Constructor 4
-    Kendaraan k4(202, "Mobil Listrik");
-    cout << "Objek 4 (Lengkap):" << endl;
-    k4.tampilkanInfo();
-
-    return 0;
-}
+    
