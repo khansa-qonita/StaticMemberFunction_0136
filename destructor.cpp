815fcd0f59e743318744f1dd7d2dc4b3c3cebb7f
khansa-qonita <khansaqonita420@gmail.com>
2026-05-05 19:03:37 +0700

    Implementasi alokasi memori dinamis pada class angka menggunakan constructor dan destructor

--- destructor.cpp
+++ destructor.cpp
@@ -10,4 +10,46 @@ private :
 public :
     angka(int); //constructor
     ~angka();  //destructor
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i){  //constructor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){  //destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "alamat array sudah dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);   //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5);   //constructor dipanggil
+    delete ptrBelajarcpp;   //destructor dipanggil
+
+    return 0;
 }
