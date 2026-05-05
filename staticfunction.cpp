36152744098b96cdedb17d643e4108a5ba5a39ad
khansa-qonita <khansaqonita420@gmail.com>
2026-05-05 18:45:53 +0700

    menampilkan NIM terakhir yang diproses

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -60,5 +60,12 @@ int main() {
     mhs3.printAll();
     mhs4.printAll();
 
+    // menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+
+    system("pause");
+    return 0;
+}
+
     
 
