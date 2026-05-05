430e37edc564e31cae740047d8f43d765af030bb
khansa-qonita <khansaqonita420@gmail.com>
2026-05-05 18:38:52 +0700

    menambahkan definisi constructor

--- constructor.cpp
+++ constructor.cpp
@@ -4,4 +4,9 @@ using namespace std;
 class mahasiswa{
 public:
     mahasiswa();  //conatructor
+};
+
+mahasiswa::mahasiswa() {
+    cout <<"constructor terpanggil" << endl;  //definisi constructor
+    
 }
